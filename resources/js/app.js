fa8b6a128b68fab426c3fa7bb539ee4e218c4945
mashuir1610 <mashuirrahamanshuvo@gmail.com>
2023-08-17 22:00:51 +0600

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+
+import jQuery from 'jquery';
+window.$ = jQuery;
+
+import swal from 'sweetalert2';
+window.Swal = swal;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <script type="module">
+
+        $('button').click(function(){
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                }
+            });
+        });
+
+    </script>
+
+</head>
+<body>
+<div id="app">
+
+    <main class="container">
+        <h1> How To Install Sweetalert2 in Laravel? - shouts.dev</h1>
+
+        <button class="btn btn-success">Click Me</button>
+    </main>
+</div>
+
+</body>
+</html>
